a41d1cf8fca38fcb599a3316e4071e97dcb3eef4
Jeff Goodall <goodallhj@gmail.com>
2017-10-15 15:05:21 -0600

    Added jquery and the first event listener in javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+var websiteTitle = document.querySelector('#website-title')
+var websiteURL = document.querySelector('#website-url')
+var enterButton = document.querySelector('#enter-button')
+
+enterButton.addEventListener('click', function (event){
+event.preventDefault()
+console.log('hi')
+})
