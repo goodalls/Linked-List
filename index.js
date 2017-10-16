25b662a89d5257520fefddf0605400dd8b8d8e22
Jeff Goodall <goodallhj@gmail.com>
2017-10-15 19:25:49 -0600

    CSS and Syling with mobile version

--- index.js
+++ index.js
@@ -5,4 +5,29 @@ var enterButton = document.querySelector('#enter-button')
 enterButton.addEventListener('click', function (event){
 event.preventDefault()
 console.log('hi')
-})
+
+});
+
+// //Katie's code:
+
+// //first, select button and set up event listener for click using jquery
+
+// $('#katies-button').on('click', function() {
+//   //second, append new cards onto the section .bookmark-wrap using jquery
+//   var websiteTitle = 'YOUR TITLE HERE';
+  
+//   $('#bookmark-wrap').append('<div class = "bookmark"><h4>' + websiteTitle + '</h4><hr><a href="http://www.thewebsiteurl.com">www.thewebsiteurl.com</a><hr><label for = "read-button" hidden></label><button class = "bookmark-button" id = "read-button">Read</button><label for = "delete-bookmark-button" hidden></label><button class = "bookmark-button" id = "delete-bookmark-button">Delete</button></div>');
+// });
+
+
+// //your original code below
+// var bookmark = document.querySelector('.bookmark');
+// var bookmarkBox = document.querySelector('#bookmark-wrap')
+
+// function doIt() {
+// // document.createElement('bookmark');
+// bookmarkBox.appendChild(bookmark);
+// console.log('screaming internally')
+// }
+
+// doIt();
