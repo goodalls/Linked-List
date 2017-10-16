90df74fbc185fb014f20b0919cdbed3139708631
Nyssa Keller <nyssa1@gmail.com>
2017-10-16 08:42:44 -0600

    Added beginning of js and javascript

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-//  javaScript to do stuff
+
+var enterButton = document.querySelector('#enter-button');
+var websiteTitleInput = document.querySelector('.website-title');
+var websiteUrlInput = document.querySelector('.website-url');
+var cardWebsiteTitle = document.querySelector('.title');
+var cardWebsiteUrl = document.querySelector('.url');
+
+$( document ).ready(function() {
+  $('#enter-button').on('click', function() {
+     $('#column2').append('<div class='cards'><h4>' + cardWebsiteTitle + '</h4><hr><a href='http://www.thewebsiteurl.com' www.thewebsiteurl.com</a><hr><button class='bookmark-button' id='read-button'>Read</button><button class='bookmark-button' id='delete-bookmark-button'>Delete</button></div>')
+     
+});     
+
+    var bookmarkList = document.querySelector('#column2');
+    var bookmark = document.querySelector('.cards');
+
+    function addBookmark() {
+    bookmarkList.appendChild(bookmark);
+    }
+
+    addBookmark();
+  }
