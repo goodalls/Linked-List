5776bffde9181f8c6ed79a1a839f527bec36d223
Nyssa Keller <nyssa1@gmail.com>
2017-10-17 12:11:35 -0600

    minor js changes

--- index.js
+++ index.js
@@ -17,44 +17,11 @@ var inputHandler = function() {
     }
    
 };
-
-<<<<<<< HEAD
 titleInput.addEventListener('keyup', inputHandler);
 
-function addCardElement (event) {
-    event.preventDefault();
-    console.log('addCardElement is working');
-    var title = titleInput.value;
-    var url = urlInput.value;
-    var newCard = document.createElement('article');
-    newCard.innerHTML = '<article class="cards"><p class="title">'+title+'</p><hr><p class="url"><a href="'+url+'">'+url+'</a></p><hr><a class="bottom-left" href="'+url+'">Read</a><a class="bottom-right" href="#">Delete</a></article>';
-    var parent = document.getElementById('column2');
-
-
-    parent.appendChild(newCard);
-
-
-};
-
-enterButton.addEventListener('click', addCardElement() {
-        
-        var amountOfCards = [];
-        function keepTrackOfAmountOfCards() {
-            var x = document.getElementById("box");
-            amountOfCards.push(document.getElementById("titleInput").value);
-            x.innerHTML = amountOfCards.join('<br/>');
-        }
-    console.log(amountOfCards);
-})
 
 
 
-
-
-
-
-
-=======
 //Card Generator javaScript
 enterButton.addEventListener('click', addCardElement);
 
@@ -85,4 +52,4 @@ function removeCard (e){
 // d.className += " after";
 
 
->>>>>>> master
+
