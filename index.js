11ca38fcc94c8db23d4170083d513925cc9d7f78
Nyssa Keller <nyssa1@gmail.com>
2017-10-18 09:02:05 -0600

    got table fully functioning

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var enterButton = document.querySelector('#enter-button');
-var inputs = document.querySelector('.inputs');
 var titleInput = document.querySelector('#website-title');
 var urlInput = document.querySelector('#website-url');
 var cardWebsiteTitle = document.querySelector('.title');
@@ -13,70 +12,100 @@ enterButton.className = 'disabled';
 
 var inputHandler = function() {
     if(document.querySelector('input').value == ""){
-       console.log('im empty');
-       enterButton.disabled = true;  
+       enterButton.disabled = true; 
+      
     } else {
         enterButton.disabled = false;
     }
    
 };
 
-inputs.addEventListener('keyup', inputHandler);
+
+$('.inputs').on('keyup', inputHandler);
 
 
 
 
 //Card Generator javaScript
-enterButton.addEventListener('click', addCardElement);
-enterButton.addEventListener('click', totalCard);
+$('#enter-button').on('click', addCardElement);
+$('#enter-button').on('click', totalCard);
 
 
 function addCardElement (event) {
-event.preventDefault()
+  event.preventDefault()
 
-var newCard = document.createElement('article');
-newCard.classList.add('cards')
-newCard.innerHTML = `
-<p class="title">${titleInput.value}</p>
-<hr>
-<p class="url"><a target="_blank" href="${urlInput.value}">${urlInput.value}</a></p>
-<hr>
-<button class="bottom-left">Read</button>
-<button class="bottom-right">Delete</button>
-`
-var whereToPutElement = document.getElementById('cards-section');
+  var newCard = document.createElement('article');
+  newCard.classList.add('cards')
 
-whereToPutElement.prepend(newCard);
+  newCard.innerHTML = `
+  <p class="title">${titleInput.value}</p>
+  <hr>
+  <p class="url"><a target="_blank" href="${urlInput.value}">${urlInput.value}</a></p>
+  <hr>
+  <button class="bottom-left">Read</button>
+  <button class="bottom-right">Delete</button>
+  `
+  var whereToPutElement = document.getElementById('cards-section');
+  whereToPutElement.prepend(newCard);
+
+  totalCard();
+  totalCardUnread();
 };
 
 
 //Removes card element when you click the delete button 
-$('#column2').on('click', removeCard);
+$('#column2').on('click', cardHandler);
 
-function removeCard (e){
+function cardHandler (e){
   if($(e.target).hasClass('bottom-right')){
     $(e.target).parent().remove();
+     totalCard();
+     totalCardRead();
+     totalCardUnread();
+  } else if ($(e.target).hasClass('bottom-left')){
+    $(e.target).parent().addClass('read');
+    totalCardRead();
+    totalCardUnread();
   }
 }
+
 
 //mark card as read by toggleing class ".after" to css
 
 
 
-classCardsSection.addEventListener('click', addClassToCSS);
+//classCardsSection.addEventListener('click', addClassToCSS);
 
 
-function addClassToCSS (event) {
-  if (event.target.className === 'bottom-left')
-  console.log('addClassToCSS is working')
-  classCardsSection.classList.toggle('after')
-};
+// function addClassToCSS (event) {
+//   if (event.target.className === 'bottom-left')
+//   console.log('addClassToCSS is working')
+//   classCardsSection.classList.toggle('after')
+// };
+
+$('.bottom-left').on('click',function(event) {
+  $('.cards').addClass('read');
+  console.log($('.cards'));
+})
 
 
-function totalCard(){
+
+var totalCard = function(){
     var totalCards = $('.cards').length
     $('.total-number-of-links').text(totalCards)
 }
+
+var totalCardRead = function(){
+    var totalCards = $('.cards.read').length
+    $('.read-links').text(totalCards)
+}
+
+var totalCardUnread = function() {
+    var totalCards = $('.cards').length - $('.cards.read').length
+    $('.unread-links').text(totalCards)
+
+}
+
 
 
 
